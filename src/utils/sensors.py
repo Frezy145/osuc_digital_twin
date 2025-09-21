4e3f3c6e64839e4dbdc2aad8379bb3f7d7a87279
Larazouna Kabissa <larazounak@gmail.com>
2025-09-21 21:09:52 +0200

    Sensors data reading up to date, 'slave' change to 'device_id' to be conform to pymodbus:23.11.2

--- src/utils/sensors.py
+++ src/utils/sensors.py
@@ -66,7 +66,7 @@ def read_csv_and_compute_mean():
 
 # Fonction pour lire les données d'une sonde donnée
 def read_sensor(client, slave_id, label=""):
-    response = client.read_holding_registers(address=0, count=8, unit=slave_id)
+    response = client.read_holding_registers(address=0, count=8, device_id=slave_id)
     if response.isError():
         log_error(f"Erreur de lecture Modbus (sonde {label}): {response}")
         return None
